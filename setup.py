13da913c681f56c94f6f8c30607fde82110c2dc1
Ray Holder <ray.holder+github@gmail.com>
2016-05-02 23:13:36 -0500

    bump up package version to match requirements.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as file_requirements:
 
 setup(
     name='sshuttle',
-    version='0.77.2',
+    version='0.78.0',
     packages=find_packages(),
     install_requires=requirements,
 )
